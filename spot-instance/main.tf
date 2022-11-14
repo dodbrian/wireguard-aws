960092a992ac6dd511edac86f7c3860967b4ec3b
Denis Zimin <roydronov@gmail.com>
2022-11-14 23:54:42 +0100

    add instance role

--- spot-instance/main.tf
+++ spot-instance/main.tf
@@ -24,6 +24,7 @@ resource "aws_instance" "wg_test_instance" {
   ami                         = data.aws_ami.ubuntu-linux.id
   associate_public_ip_address = true
   security_groups             = [aws_security_group.wg_test_security_group.name]
+  iam_instance_profile        = aws_iam_instance_profile.wg_test_instance_profile.name
   key_name                    = "dodbrian-eu-north-1"
   user_data                   = filebase64("user-data.sh")
 
@@ -36,6 +37,11 @@ resource "aws_instance" "wg_test_instance" {
   }
 }
 
+resource "aws_iam_instance_profile" "wg_test_instance_profile" {
+  name = "wg_test_instance_profile"
+  role = aws_iam_role.wg_test_role.name
+}
+
 resource "aws_security_group" "wg_test_security_group" {
   name        = "wg_test_security_group"
   description = "WireGuard security group"
@@ -45,7 +51,7 @@ resource "aws_security_group" "wg_test_security_group" {
   }
 
   ingress {
-    cidr_blocks = ["84.62.226.198/32"]
+    cidr_blocks = ["178.6.229.122/32"]
     description = "SSH"
     from_port   = 22
     to_port     = 22
@@ -74,6 +80,45 @@ resource "aws_security_group" "wg_test_security_group" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+  }
+}
+
+resource "aws_iam_role" "wg_test_role" {
+  name        = "wg_test_role"
+  description = "Allows EC2 instances to call AWS services on your behalf."
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "sts:AssumeRole"
+        ],
+        "Principal" : {
+          "Service" : [
+            "ec2.amazonaws.com"
+          ]
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "wg_test_s3_policy"
+
+    policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:*"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    })
   }
 }
 
